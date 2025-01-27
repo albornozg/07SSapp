a432facaad4e8a2decce4d2547f5a4d6489cb932
Gabriel Albornoz <albornozbg97@gmail.com>
2025-01-27 13:18:48 -0400

    Same as before. Missed these files

--- SS_test_files/testapp.py
+++ SS_test_files/testapp.py
@@ -5,72 +5,189 @@ import random
 import win32api, win32con
 import cv2
 import numpy as np
+from tkinter import *
+from tkinter import ttk
+from function_files import *
 # import PySimpleGUI as sg
 
-# First square would be on 106,11 ; 149,53 ; w=53 , h=36
-width = 233
-height = 320
+"""
+def select_screen():
+    root = Tk()
+    root.title("07 Screenshotter")
 
-# definimos la funcion para la toma de captura de pantalla
-def base_ss():
-    im1_location = pyautogui.locateOnScreen('SS_sword-v.1.png', grayscale=True, confidence=0.8)
-    im2_location = pyautogui.locateOnScreen('SS_quest-v.2.png', grayscale=True, confidence=0.7)
-    im3_location = pyautogui.locateOnScreen('SS_inventory-v.3.png', grayscale=True, confidence=0.7)
+    content = ttk.Frame(root)
+    content.grid(column=0, row=0)
 
-    x_base = int(im1_location.left)
-    y_base = int(im1_location.top)
+    label_1 = ttk.Label(content, text='Select the skills you would like to have hidden:')
+    label_1.grid(column=0, row=0)
 
-
-    im2_loc_point = pyautogui.center(im2_location)
-    im3_loc_point = pyautogui.center(im3_location)
-
-    s1 = pyautogui.screenshot(region=(x_base, y_base, width, height))
-    s1 = np.array(s1)
+    frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=2000, height=1000)
+    frame.grid(column=0, row=1)
 
 
-    pyautogui.click(im2_loc_point.x, im2_loc_point.y)
-    time.sleep(0.25)
-    s2 = pyautogui.screenshot(region=(x_base, y_base, width, height))
-    
-    if ach_dia == 'Yes':
-        pyautogui.click(im2_loc_point.x + 120, im2_loc_point.y + 45)
-        time.sleep(0.5)
-        s4 = pyautogui.screenshot(region=(x_base, y_base, width, height))
-        time.sleep(0.5)
-        pyautogui.click(im2_loc_point.x + 146, im2_loc_point.y + 314)
-        s5 = pyautogui.screenshot(region=(x_base, y_base, width, height))
-    else:
-        pass
-    
-    s2 = np.array(s2)
 
-    time.sleep(0.25)
+    rcvar = BooleanVar(value=False)
+    constvar = BooleanVar(value=False)
+    agivar = BooleanVar(value=False)
+    hervar = BooleanVar(value=False)
+    thievar = BooleanVar(value=False)
+    crafvar = BooleanVar(value=False)
+    fletvar = BooleanVar(value=False)
+    slayvar = BooleanVar(value=False)
+    huntvar = BooleanVar(value=False)
+    minvar = BooleanVar(value=False)
+    smivar = BooleanVar(value=False)
+    fishvar = BooleanVar(value=False)
+    cookvar = BooleanVar(value=False)
+    firevar = BooleanVar(value=False)
+    woodvar = BooleanVar(value=False)
+    farvar = BooleanVar(value=False)
+    ach_dia = BooleanVar(value=False)
 
-    pyautogui.click(im3_loc_point.x, im3_loc_point.y)
-    s3 = pyautogui.screenshot(region=(x_base, y_base, width, height))
-    s3 = np.array(s3)
+    rc = ttk.Checkbutton(frame, text="Runecraft", variable=rcvar, onvalue=True)
+    const = ttk.Checkbutton(frame, text="Construction", variable=constvar, onvalue=True)
+    agi = ttk.Checkbutton(frame, text="Agility", variable=agivar, onvalue=True)
+    herb = ttk.Checkbutton(frame, text="Herblore", variable=hervar, onvalue=True)
+    thie = ttk.Checkbutton(frame, text="Thieving", variable=thievar, onvalue=True)
+    craf = ttk.Checkbutton(frame, text="Crafting", variable=crafvar, onvalue=True)
+    flet = ttk.Checkbutton(frame, text="Fletching", variable=fletvar, onvalue=True)
+    slay = ttk.Checkbutton(frame, text="Slayer", variable=slayvar, onvalue=True)
+    hunt = ttk.Checkbutton(frame, text="Hunter", variable=huntvar, onvalue=True)
+    min = ttk.Checkbutton(frame, text="Mining", variable=minvar, onvalue=True)
+    smi = ttk.Checkbutton(frame, text="Smithing", variable=smivar, onvalue=True)
+    fish = ttk.Checkbutton(frame, text="Fishing", variable=fishvar, onvalue=True)
+    cook = ttk.Checkbutton(frame, text="Cooking", variable=cookvar, onvalue=True)
+    fire = ttk.Checkbutton(frame, text="Firemaking", variable=firevar, onvalue=True)
+    wood = ttk.Checkbutton(frame, text="Woodcutting", variable=woodvar, onvalue=True)
+    far = ttk.Checkbutton(frame, text="Farming", variable=farvar, onvalue=True)
 
-    ss_BGR = np.concatenate((s3, s1, s2), axis=1)
-    ss_RGB = cv2.cvtColor(ss_BGR, cv2.COLOR_BGR2RGB)
-    ss_BGR_ad = np.concatenate((s3, s1, s2, s4, s5), axis=1)
-    ss_RGB_ad = cv2.cvtColor(ss_BGR_ad, cv2.COLOR_BGR2RGB)
+    achiev = ttk.Checkbutton(content, text="Include Achievement Diaries?", variable=ach_dia, onvalue=True)
 
-        
-    alert(text='Pictures have  been taken!', title='Complete', button='OK')   
-    if ach_dia == 'Yes':
-        return cv2.imwrite('Screenshot.png', ss_RGB_ad)
-    else:
-        return cv2.imwrite('Screenshot.png', ss_RGB)
+    rc.grid(column=0, row=0, sticky="w")
+    const.grid(column=0, row=1, sticky="w")
+    agi.grid(column=0, row=2, sticky="w")
+    herb.grid(column=0, row=3, sticky="w")
+    thie.grid(column=1, row=0, sticky="w")
+    craf.grid(column=1, row=1, sticky="w")
+    flet.grid(column=1, row=2, sticky="w")
+    slay.grid(column=1, row=3, sticky="w")
+    hunt.grid(column=2, row=0, sticky="w")
+    min.grid(column=2, row=1, sticky="w")
+    smi.grid(column=2, row=2, sticky="w")
+    fish.grid(column=2, row=3, sticky="w")
+    cook.grid(column=3, row=0, sticky="w")
+    fire.grid(column=3, row=1, sticky="w")
+    wood.grid(column=3, row=2, sticky="w")
+    far.grid(column=3, row=3, sticky="w")
 
+    achiev.grid(column=0, row=4, columnspan=3)
+
+    root.mainloop()
+"""
+
+"""
+root = Tk()
+root.title("Welcome!")
+
+welcome_label = ttk.Label(root, text='Welcome to the screenshotter version 1. This simple app was created by Gabriel Albornoz')
+welcome_label.grid(column=0, row=0)
+
+okay_button = ttk.Button(root, text='Okay', command=select_screen)
+cancel_button = ttk.Button(root, text='Cancel', command=root.destroy)
+
+okay_button.grid(column=0, row=1)
+cancel_button.grid(column=1, row=1)
+
+
+
+root.mainloop()
+"""
+# tkinter test
+
+root = Tk()
+root.title("07 Screenshotter")
+
+content = ttk.Frame(root)
+content.grid(column=0, row=0)
+
+label_1 = ttk.Label(content, text='Select the skills you would like to have hidden:')
+label_1.grid(column=0, row=0)
+
+frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=2000, height=1000)
+frame.grid(column=0, row=1)
+
+
+
+rcvar = BooleanVar(value=False)
+constvar = BooleanVar(value=False)
+agivar = BooleanVar(value=False)
+hervar = BooleanVar(value=False)
+thievar = BooleanVar(value=False)
+crafvar = BooleanVar(value=False)
+fletvar = BooleanVar(value=False)
+slayvar = BooleanVar(value=False)
+huntvar = BooleanVar(value=False)
+minvar = BooleanVar(value=False)
+smivar = BooleanVar(value=False)
+fishvar = BooleanVar(value=False)
+cookvar = BooleanVar(value=False)
+firevar = BooleanVar(value=False)
+woodvar = BooleanVar(value=False)
+farvar = BooleanVar(value=False)
+ach_dia = BooleanVar(value=False)
+
+rc = ttk.Checkbutton(frame, text="Runecraft", variable=rcvar, onvalue=True)
+const = ttk.Checkbutton(frame, text="Construction", variable=constvar, onvalue=True)
+agi = ttk.Checkbutton(frame, text="Agility", variable=agivar, onvalue=True)
+herb = ttk.Checkbutton(frame, text="Herblore", variable=hervar, onvalue=True)
+thie = ttk.Checkbutton(frame, text="Thieving", variable=thievar, onvalue=True)
+craf = ttk.Checkbutton(frame, text="Crafting", variable=crafvar, onvalue=True)
+flet = ttk.Checkbutton(frame, text="Fletching", variable=fletvar, onvalue=True)
+slay = ttk.Checkbutton(frame, text="Slayer", variable=slayvar, onvalue=True)
+hunt = ttk.Checkbutton(frame, text="Hunter", variable=huntvar, onvalue=True)
+min = ttk.Checkbutton(frame, text="Mining", variable=minvar, onvalue=True)
+smi = ttk.Checkbutton(frame, text="Smithing", variable=smivar, onvalue=True)
+fish = ttk.Checkbutton(frame, text="Fishing", variable=fishvar, onvalue=True)
+cook = ttk.Checkbutton(frame, text="Cooking", variable=cookvar, onvalue=True)
+fire = ttk.Checkbutton(frame, text="Firemaking", variable=firevar, onvalue=True)
+wood = ttk.Checkbutton(frame, text="Woodcutting", variable=woodvar, onvalue=True)
+far = ttk.Checkbutton(frame, text="Farming", variable=farvar, onvalue=True)
+
+achiev = ttk.Checkbutton(content, text="Include Achievement Diaries?", variable=ach_dia, onvalue=True)
+
+rc.grid(column=0, row=0, sticky="w")
+const.grid(column=0, row=1, sticky="w")
+agi.grid(column=0, row=2, sticky="w")
+herb.grid(column=0, row=3, sticky="w")
+thie.grid(column=1, row=0, sticky="w")
+craf.grid(column=1, row=1, sticky="w")
+flet.grid(column=1, row=2, sticky="w")
+slay.grid(column=1, row=3, sticky="w")
+hunt.grid(column=2, row=0, sticky="w")
+min.grid(column=2, row=1, sticky="w")
+smi.grid(column=2, row=2, sticky="w")
+fish.grid(column=2, row=3, sticky="w")
+cook.grid(column=3, row=0, sticky="w")
+fire.grid(column=3, row=1, sticky="w")
+wood.grid(column=3, row=2, sticky="w")
+far.grid(column=3, row=3, sticky="w")
+
+achiev.grid(column=0, row=4, columnspan=3)
+
+root.mainloop()
+
+
+"""
 okc = confirm(text='Welcome to the remastered version. This simple app was created by Gabriel Albornoz', title='Initio', buttons=['OK', 'Cancel'])
 ach_dia = confirm(text='Do you want to include the Achievement Diaries into the image?', title='Achievement Diaries?', buttons=['Yes', 'No'])
 
 if okc == 'OK':
     try:
-        base_ss()
+        base_ss(ach_dia)
         
     except pyautogui.ImageNotFoundException:
         alert(text='Runelite not detected. Try again.', title='Warning', button='OK')
         exit      
 else:
     exit
+"""
